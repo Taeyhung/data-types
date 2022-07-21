e166e698b096e0c5d651d113758a0d5e5edff996
Taehyung <danielasinobi10@gmail.com>
2022-07-21 13:56:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello")
+let w="orange"
+console.log(w)
+let Daniel="Daniel"+" "+"Asinobi"
+console.log(Daniel)
+let word= 2 +"chidera"
+console.log(word)
+let number= 2 + '2'
+console.log(number)
+let n= 2 + 2
+console.log(n)
+let x= 20
+let y= 5
+let multiply= x*y
+console.log(multiply)
+let add= x+y
+console.log(add)
+let subtract= x-y
+console.log(subtract)
+let divide= x/y
+console.log(divide)
+let equals = x==y
+console.log(equals)
+let notequals= x!=y
+console.log(notequals)
+let win= x>y
+console.log(win)
+let lose= w<y
+console.log(lose)
