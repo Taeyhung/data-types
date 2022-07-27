1b45280a1a9f5d20c3718e3895cc00db972270bf
Taehyung <danielasinobi10@gmail.com>
2022-07-27 12:09:48 +0100

    changed the code

--- index.js
+++ index.js
@@ -27,3 +27,29 @@ let win= x>y
 console.log(win)
 let lose= w<y
 console.log(lose)
+let emptyArray=[]
+let numbers= [1,2,3,]
+console.log(numbers)
+console.log(numbers[2])
+let randomArray= [1,"Best",true,null]
+console.log(randomArray[3])
+console.log(numbers.length)
+console.log(emptyArray.length)
+console.log(randomArray.length)
+let newArray= [5,6,1,2,5,6,7,8,10,100,0,13]
+let lastItemPosition= newArray.length-1
+console.log(newArray[lastItemPosition])
+
+let newObject= {}
+let personObject= {
+    title: "Daniel",
+    age: 12,
+    job: "Footballer",
+    isRich: true,
+}
+console.log(personObject)
+console.log(personObject["title"])
+console.log(personObject.job)
+console.log(personObject.age)
+console.log(personObject["age"])
+console.log(personObject["job"])
